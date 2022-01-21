dc4b59285dc9b7990903257c849cfc67d1880b6d
absurdismworld <19274382+absurdismworld@users.noreply.github.com>
2022-01-21 20:56:01 +0100

    Add files via upload
    Preparations for migrating to GitHub (part 1)

---
+++ garalinasneakpeakpasswordgen/helper.js
@@ -0,0 +1,13 @@
+<script>
+  $(window).load(function () {
+    $(".trigger_popup_fricc").click(function(){
+       $('.hover_bkgr_fricc').show();
+    });
+    $('.hover_bkgr_fricc').click(function(){
+        $('.hover_bkgr_fricc').hide();
+    });
+    $('.popupCloseButton').click(function(){
+        $('.hover_bkgr_fricc').hide();
+    });
+});
+/script>
