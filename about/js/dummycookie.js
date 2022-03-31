dd0ad3571def084cdbfa602cc4a0c95472ca94f1
absurdismworld <19274382+absurdismworld@users.noreply.github.com>
2022-03-31 19:47:57 +0200

    Add files via upload

---
+++ about/js/dummycookie.js
@@ -0,0 +1,10 @@
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date(1);
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    return "";
+}
+
+/*these pretty much do nothing i but i can't be arsed to rewrite that one part*/
